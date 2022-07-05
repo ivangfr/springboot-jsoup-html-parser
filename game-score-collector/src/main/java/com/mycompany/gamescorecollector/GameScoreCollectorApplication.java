719ed0228f4a5e3d79e672ec5233df57496f0918
Ivan Franchin <ivan.franchin@takeaway.com>
2022-07-05 17:54:54 +0200

    set EnableMongoAuditing annotation in a specifc MongoConfig class

--- game-score-collector/src/main/java/com/mycompany/gamescorecollector/GameScoreCollectorApplication.java
+++ game-score-collector/src/main/java/com/mycompany/gamescorecollector/GameScoreCollectorApplication.java
@@ -2,11 +2,9 @@ package com.mycompany.gamescorecollector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.nativex.hint.NativeHint;
 
 @NativeHint(options = "--enable-url-protocols=http,https")
-@EnableMongoAuditing
 @SpringBootApplication
 public class GameScoreCollectorApplication {
 
---
+++ game-score-collector/src/main/java/com/mycompany/gamescorecollector/config/MongoConfig.java
@@ -0,0 +1,9 @@
+package com.mycompany.gamescorecollector.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@EnableMongoAuditing
+@Configuration
+public class MongoConfig {
+}
